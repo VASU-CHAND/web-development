ec1ebb5265ec1277b35331464caaeb8e41b644fe
VASU CHAND <vasuchand9@gmail.com>
2026-04-09 19:30:39 +0530

    add

---
+++ javascript/practice/practiceqs1.js
@@ -0,0 +1,14 @@
+let light = prompt("enter the color of light");
+if(light==="red"){
+    console.log("stop");
+
+}
+else if(light==="yellow"){
+    console.log("ready");
+}
+else if(light==="green"){
+    console.log("go");
+}
+else{
+    console.log("error");
+}
