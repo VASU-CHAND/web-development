5619373933103703a7b20a7db9b80c11db70f161
VASU CHAND <vasuchand9@gmail.com>
2026-06-17 14:23:03 +0530

    add

---
+++ javascript/part5/getvalues.js
@@ -0,0 +1,6 @@
+let student={
+    name:"vasu",
+    marks :94.4
+}
+console.log(student["name"])
+console.log(student.name)
--- javascript/part5/objectlitrals.js
+++ javascript/part5/objectlitrals.js
@@ -8,3 +8,11 @@ let delhi={
 //
 //         key 2: valie
 //            }
+const post ={
+username : "@shradhakhapra",
+content : "this is my #firstPost",
+likes : 150,
+reposts:5,
+tags : ["@apnacollege", "@hero"]
+
+}
