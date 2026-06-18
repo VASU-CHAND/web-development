9f975aee090ce328531d43935583ea23060ef0c3
VASU CHAND <vasuchand9@gmail.com>
2026-06-18 08:53:04 +0530

    add

---
+++ javascript/part5/guessinggame.js
@@ -0,0 +1,5 @@
+randomnum =Math.floor(Math.random()*10)
+while(true){
+    let number= prompt("enter the number between 1 to 10")
+
+}
---
+++ javascript/part6/avgcalculator.js
@@ -0,0 +1,4 @@
+function avg(a ,b){
+    console.lof((a+b)/2)
+}
+console.log(avg(2,8))
